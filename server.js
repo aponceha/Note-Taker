25d7b5f77bd1fc18688164ac251830c66d6570b3
aponceha <114960304+aponceha@users.noreply.github.com>
2023-03-22 18:32:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
